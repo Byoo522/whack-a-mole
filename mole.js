a1a74a67a2be70a65bd249443d0801ceb4733eaa
Ben <byoo0522@gmail.com>
2021-05-05 13:50:06 -0700

    step 3 complete

--- mole.js
+++ mole.js
@@ -10,28 +10,54 @@
 //   });
 
 
-window.addEventListener("DOMContentLoaded", () => {
-    function popUpRandomMole() {
 
-        let moleHeads = document.getElementsByClassName('wgs__mole-head');
-        let randomNum = getRandomIntInclusive(0, 7);
-        let currentMole = moleHeads[randomNum]
-        currentMole.classList.remove('wgs__mole-head--hidden')
+function popUpRandomMole() {
+    let moleHeads = document.getElementsByClassName('wgs__mole-head');
 
-        setTimeout(() => {
-            currentMole.classList.add('wgs__mole-head--hidden')
-        }, 1000);
-    }
+    let randomNum = getRandomIntInclusive(0, 7);
+    let currentMole = moleHeads[randomNum]
+    currentMole.classList.remove('wgs__mole-head--hidden')
+
+    setTimeout(() => {
+        hideMole(currentMole);
+    }, 3000);
+}
 
 
-    function getRandomIntInclusive(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-    }
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
 
-    setInterval(() => {
+
+
+
+
+function hideMole(mole) {
+    mole.classList.add('wgs__mole-head--hidden')
+    setTimeout(() => {
         popUpRandomMole();
-    },1000)
+    }, 3000)
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        popUpRandomMole();
+    }, 0)
+
+    let moleHeads = document.querySelectorAll('.wgs__mole-head');
+
+    // iterate through moleheads
+    // click event listener that calls moleheads when molehead clicked
+    // add --hidden
+
+    moleHeads.forEach(mole => {
+        mole.addEventListener('click', event => {
+            hideMole(mole);
+        })
+
+    })
+
 
 })
